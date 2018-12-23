6ee8aad1d6d80860d762bd2c8cc9c7ccfa3a20b0
csouriss <christophe.sourisse@capgemini.com>
2018-12-23 20:16:31 +0100

    Correcting messages displayed when no Conemu dir is found when installing

--- conemu/install.sh
+++ conemu/install.sh
@@ -12,12 +12,11 @@ EOF
 }
 
 find_tools_dir() {
-  if [ -d $DOTBASHCFG_DATA_DIR/tools ]; then
+  if [ -d "$DOTBASHCFG_DATA_DIR/tools" ]; then
     echo "$DOTBASHCFG_DATA_DIR/tools"
-  elif [ -d $DOTBASHCFG_DATA_DIR/utils ]; then
+  elif [ -d "$DOTBASHCFG_DATA_DIR/utils" ]; then
     echo "$DOTBASHCFG_DATA_DIR/utils"
   else
-    (>&2 echo "Unable to find path for $1.")
     (>&2 echo "None of the following directories exist:")
     (>&2 echo "- $DOTBASHCFG_DATA_DIR/tools")
     (>&2 echo "- $DOTBASHCFG_DATA_DIR/utils")
@@ -26,7 +25,7 @@ find_tools_dir() {
 }
 
 find_as_windows_path() {
-  find $1 -iname "$2" | head -1 | sed 's#^/\([a-z]\)#\U\1:#'
+  find "$1" -iname "$2" | head -1 | sed 's#^/\([a-z]\)#\U\1:#'
 }
 
 replace() {
@@ -39,15 +38,16 @@ replace() {
 
 install() {
   tools_dir="$(find_tools_dir)"
-  conemu_exe="$(find "$tools_dir" -name 'ConEmu.exe' | head -1)"
+  [ $? -eq 0 ] && conemu_exe="$(find "$tools_dir" -name 'ConEmu.exe' | head -1)"
+
   if [ -f "$conemu_exe" ]; then
-    conemu_config_file="`dirname $conemu_exe`/ConEmu.xml"
+    conemu_config_file="$(dirname "$conemu_exe")/ConEmu.xml"
 
     echo "Setting ConEmu configuration into $conemu_config_file file..."
-    cp conemu/ConEmu.xml $conemu_config_file
+    cp conemu/ConEmu.xml "$conemu_config_file"
 
     echo "Configuring $conemu_config_file file..."
-    replace "%CygwinHome%" "$(find_as_windows_path "$tools_dir" Cygwin64)" $conemu_config_file
+    replace "%CygwinHome%" "$(find_as_windows_path "$tools_dir" Cygwin64)" "$conemu_config_file"
 
   else
     echo "No ConEmu installation directory found. Aborting ConEmu feature configuration."
