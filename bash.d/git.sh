c0c92d73c0a6c6bf3fcad2a37be779a8aeeb19f2
csouriss <christophe.sourisse@capgemini.com>
2018-12-23 20:13:05 +0100

    Correcting git_fetch_remotes alias to properly create new git branches after fetch (+ shellcheck corrections)

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -3,9 +3,16 @@
 # Make sure that all remote branches have some local equivalent one
 # and set each of them to track its remote associated origin.
 git_fetch_remotes() {
+  # Fetch remote branches and prune (delete) obsolete local branches 
   git fetch origin -p
+
+  # Create new local branches accordingly
+  local_branches="$(git branch -l | sed 's:\s*\(\*\)\?\s*::')"
   git branch -r | egrep -v 'master$' | while read remote_b; do 
-    git branch --set-upstream-to=$remote_b "${remote_b#origin/}"
+    local_b=${remote_b#origin/}
+    if ! ( echo "$local_branches" | egrep -q "^${local_b}$" ); then
+      git branch "${local_b}" "${remote_b}"
+    fi
   done
 }
 
@@ -26,9 +33,13 @@ git_branch_state() {
 # Trigger this function on each new prompt entry to re-calculate
 # Git branch state for the current working directory
 git_prompt_command() {
+  # shellcheck disable=SC2155 
+  # Return code isn't used here
   local new_pwd="$(pwd)"
   if [ "$new_pwd" != "$DOTBASHCFG_LAST_PWD" ]; then
     if git rev-parse --is-inside-work-tree 2>/dev/null 1>&2; then
+      # shellcheck disable=SC2034 
+      # Variable DOTBASHCFG_GIT_BRANCH_STATE is used in EXTRA_PS1
       DOTBASHCFG_GIT_BRANCH_STATE="$(git_branch_state 2>/dev/null | sed 's/\(.\+\)/\(\1\)/')"
     else
       unset DOTBASHCFG_GIT_BRANCH_STATE
@@ -46,5 +57,7 @@ PROMPT_COMMAND="git_prompt_command; $PROMPT_COMMAND"
 
 # Set EXTRA_PS1 variable (used by dotbashconfig's bash_prompt.sh script)
 # to display the current git branch state (branch name or tag or commit ID)
+# shellcheck disable=SC2016 disable=SC2034
+# (EXTRA_PS1 is a contribution to PS1, used by another script)
 EXTRA_PS1='$(command -v git > /dev/null && echo "$DOTBASHCFG_GIT_BRANCH_STATE" )'
 
