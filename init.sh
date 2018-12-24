50f61986ba5c7b28ed5c71a10dcdaf4de1b36e93
csouriss <christophe.sourisse@capgemini.com>
2018-12-24 14:37:49 +0100

    Init.sh script options modified for more readability.
    * When no option is provided, the script asks the user before performing
    any installation action,
    * --no-*-feature options are replaced by --with-*-feature

--- init.sh
+++ init.sh
@@ -4,6 +4,11 @@
 
 DOTBASH_CFG_FILE=$HOME/.dotbashcfg
 
+# Build this script's effective parent directory
+# by resolving links and/or trailing ~
+DOTBASH_CFG_DIR="$(readlink -f "$(dirname "$0")")"
+DOTBASH_CFG_DIR="${DOTBASH_CFG_DIR/#\~/$HOME}"
+
 
 # -- Functions -- #
 
@@ -23,13 +28,13 @@ usage() {
 	  project, 3rd party tool installation, IDE installation, etc. (used e.g. 
 	  on Windows to find Cygwin installation path).
 
-  --no-<feature-name>-feature
-  --skip-features=<feature-1>,<feature-2>,...,<feature-n>
+  --with-<feature-name>-feature
+  --with-features=<feature-1>,<feature-2>,...,<feature-n>
 	  Skip the installation of some given dotbashconfig features (identified by 
 	  their "feature-name"). Here is the list of those features:
-	      `find . -maxdepth 2 -name install.sh -printf "%h, " | sed -e 's#./##g' -e 's#, $##g'`
+	      $(find . -maxdepth 2 -name install.sh -printf "%h, " | sed -e 's#./##g' -e 's#, $##g')
 
-  --no-feature|--skip-all-features
+  -y|--all-features
 	  Skip the installation of every dotbashconfig features (i.e. only bashrc
 	  environment will be configured).
 
@@ -42,8 +47,10 @@ EOU
 # Run the provided command & arguments into do.bashconfig project's directory
 run_in_project() {
   set -e
-  pushd "$(dirname $0)" > /dev/null
+  pushd "$DOTBASH_CFG_DIR" > /dev/null
   trap "popd > /dev/null" EXIT
+  # shellcheck disable=SC2068
+  # Array expansion is intended this way
   $@
 }
 
@@ -52,11 +59,7 @@ run_in_project() {
 # - Otherwise, exit in error.
 remove_or_abort() {
   if [ -e "$1" ]; then
-    echo "Existing $1 detected..."
-    while ! [[ $REPLY =~ ^[yn]$ ]]; do
-      read -p "Override? [y/n] " -r
-    done
-    if [ $REPLY = y ]; then
+    if ok_to "Existing $1 detected..." "Override?"; then
       rm -rf "$1"
     else
       return 2
@@ -64,29 +67,35 @@ remove_or_abort() {
   fi
 }
 
+# Ask the user a yes/no question and pick up his/her answer.
+# $1 - Preamble message (optional)
+# $2 - Main prompt message
+ok_to() {
+  [ $# -eq 2 ] && ( echo "$1"; shift )
+  REPLY=""
+  while ! [[ $REPLY =~ ^[yn]$ ]]; do
+    read -p "$1 [y/n] " -r
+  done
+  [ "$REPLY" = y ]
+  return $?
+}
+
 # Initializes.bash configuration
 init() {
-  # Build this script's effective parent directory
-  # by resolving links and/or trailing ~
-  # (using pwd since we are necessarily in dotbashconfig dir,
-  # and because, due to run_in_project, '$(dirname $0)' would be wrong)
-  real_dirname="`readlink -f $(pwd)`"
-  real_dirname="${real_dirname/#\~/$HOME}"
-
   # If this script isn't launched from ~/.bash dir, then force rebuilding 
   # ~/.bash from the actual dotbashconfig project directory
   echo "Init $HOME/.bash..."
-  if ! [ "$real_dirname" = "$(readlink -f $HOME/.bash)" ] ; then
+  if ! [ "$DOTBASH_CFG_DIR" = "$(readlink -f "$HOME/.bash")" ] ; then
     remove_or_abort "$HOME/.bash"
-    ln -s "$real_dirname" "$HOME/.bash"
+    ln -s "$DOTBASH_CFG_DIR" "$HOME/.bash"
   fi
 
   # Erase existing .bashrc, and warn the user about it first
   echo "Init $HOME/.bashrc..."
   if ! [ -e "$HOME/.bashrc" ] || ! [ -L "$HOME/.bashrc" ] || \
-	! [ "$real_dirname/bashrc" = "$(readlink -f $HOME/.bashrc)" ]; then
+	! [ "$DOTBASH_CFG_DIR/bashrc" = "$(readlink -f "$HOME/.bashrc")" ]; then
     remove_or_abort "$HOME/.bashrc"
-    ln -s $HOME/.bash/bashrc $HOME/.bashrc
+    ln -s "$HOME/.bash/bashrc" "$HOME/.bashrc"
   fi
 
   # Create a .dotbashcfg file holding defined DOTBASH_* variables
@@ -97,18 +106,33 @@ DOTBASHCFG_DATA_DIR="$DOTBASHCFG_DATA_DIR"
 EOC
 
   # Source the bashrc script
-  source $HOME/.bashrc
+  source "$HOME/.bashrc"
 }
 
-# Locate and execute every dotbashconfig feature installation script, with
-# skipping of the features that are deactivated based on this script's options.
+# Check whether some feature shall be installed or not.
+# $1 - The feature to check
+check_feature() {
+  feature="$1"
+  if [ "$RUN_ALL_FEATURES" = true ]; then
+    return 0
+  elif [ "$AUTO_DOTBASH_CFG" = true ]; then
+    run_feature_var_name=RUN_${feature^^}_FEATURE
+    [ "${!run_feature_var_name}" = true ]
+    return $?
+  else
+    ok_to "Install $feature feature?"
+    return $?
+  fi
+}
+
+# Locate and execute every dotbashconfig feature installation script that has
+# been activated (based on this script's options).
 install_features() {
   for install_script in */install.sh; do
-    feature=$(basename $(dirname $install_script))
-    skip_feature_var_name=SKIP_${feature^^}_FEATURE
-    if ! [ "${!skip_feature_var_name}" = true ]; then
+    feature=$(basename "$(dirname "$install_script")")
+    if check_feature "$feature"; then
       echo "Install / Configure $feature feature..."
-      run_in_project $install_script
+      run_in_project "$install_script"
     fi
   done
 }
@@ -133,17 +157,20 @@ while [ $# -ne 0 ]; do
     "-d"|"--data")
       shift; DOTBASHCFG_DATA_DIR="${1:-DOTBASHCFG_DATA_DIR}"
       ;;
-    --no-*-feature)
-      feature=`expr $1 : '--no-\(.*\)-feature'`
-      declare "SKIP_${feature^^}_FEATURE=true"
+    --with-*-feature)
+      feature=$(expr "$1" : '--with-\(.*\)-feature')
+      declare "RUN_${feature^^}_FEATURE=true"
+      AUTO_DOTBASH_CFG=true
       ;;
-    --skip-features=*)
-      for feature in `echo ${1// /} | cut -d= -f2 | sed 's/,/ /g'`; do
-	declare "SKIP_${feature^^}_FEATURE=true"
+    --with-features=*)
+      for feature in $(echo "${1// /}" | cut -d= -f2 | sed 's/,/ /g'); do
+	declare "RUN_${feature^^}_FEATURE=true"
       done
+      AUTO_DOTBASH_CFG=true
       ;;
-    "--no-feature"|"--skip-all-features")
-      SKIP_ALL_FEATURES=true
+    "-y"|"--all-features")
+      RUN_ALL_FEATURES=true
+      AUTO_DOTBASH_CFG=true
       ;;
     "-h"|"-?"|"--help")
       run_in_project usage; exit 0
@@ -159,10 +186,8 @@ done
 echo "Init bash environment..."
 run_in_project init
 
-# Install each dotbashconfig feature (unless deactivated)
-if ! [ "${SKIP_ALL_FEATURES}" = true ]; then
-  run_in_project install_features
-fi
+# Install each dotbashconfig feature
+run_in_project install_features
 
 # Done!
 echo "Done! Enjoy bashing ;-)"
