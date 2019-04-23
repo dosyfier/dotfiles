509c104ce6d7f0b77099749ca68e803b12da8a72
csouriss <christophe.sourisse@capgemini.com>
2019-04-23 11:09:24 +0000

    Vim 8.x support on CentOS

--- vim/feature_mgr.sh
+++ vim/feature_mgr.sh
@@ -3,6 +3,15 @@
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
+get_dependencies() {
+  echo "git"
+}
+
+install_centos() {
+  install_packages make
+  _compile
+}
+
 install_ubuntu() {
   install_repo jonathonf/vim
   # N.B. vim-gtk is a compiled vim version providing support for system clipboard
@@ -11,6 +20,16 @@ install_ubuntu() {
 
 install_winbash() {
   install_ubuntu
+}
+
+_compile() {
+  sudo git clone https://github.com/vim/vim /usr/local/src/vim
+  pushd "/usr/local/src/vim" > /dev/null
+  trap "popd > /dev/null" EXIT
+  sudo git checkout v8.1.1198
+  sudo rm -rf .git
+  sudo make
+  sudo make install
 }
 
 main "$@"
