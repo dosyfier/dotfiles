9428a7201624f48a126dbf5956ce4ddea8a7e744
csouriss <christophe.sourisse@capgemini.com>
2019-04-23 11:06:18 +0000

    Git support enhancement
    * Missing var env FEATURE_ROOT
    * Do not attempt to reinstall Git if an existing and more recent version
    * already exists

--- git/feature_mgr.sh
+++ git/feature_mgr.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # shellcheck source=../internal/install-base.sh
+FEATURE_ROOT="$(realpath "$(dirname "$0")")"
 source "$(dirname "$0")/../internal/install-base.sh"
 
 install_centos() {
-  install_repo centos-release-scl epel-release
-  _install_rh_git_29
+  if ! _is_installed; then
+    install_repo centos-release-scl epel-release
+    _install_rh_git_29
+  fi
   _configure
 }
 
 install_redhat() {
-  install_repo rhel-server-rhscl-7-rpms
-  _install_rh_git_29
+  if ! _is_installed; then
+    install_repo rhel-server-rhscl-7-rpms
+    _install_rh_git_29
+  fi
   _configure
 }
 
@@ -23,13 +28,29 @@ _install_rh_git_29() {
 }
 
 install_ubuntu() {
-  install_repo git-core/ppa
-  install_packages git gitk
+  if ! _is_installed; then
+    install_repo git-core/ppa
+    install_packages git gitk
+  fi
   _configure
 }
 
 install_winbash() {
   install_ubuntu
+}
+
+_format_version_for_cmp() { 
+  echo "$1" | sed 's|\.| |g' | xargs printf "%02d.%02d" 
+}
+
+_is_installed() {
+  installed_git_version=$(git --version 2> /dev/null | awk '{ print $NF }')
+  if [ -n "$installed_git_version" ] && \
+    [[ "$(_format_version_for_cmp "$installed_git_version")" > "$(_format_version_for_cmp "2.9")" ]]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 _configure() {
