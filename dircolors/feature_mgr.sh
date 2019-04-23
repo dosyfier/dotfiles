6935b0967c8a0b2ea984893886057b8915af24d8
csouriss <christophe.sourisse@capgemini.com>
2019-04-23 11:04:06 +0000

    Dircolors support for CentOS

--- dircolors/feature_mgr.sh
+++ dircolors/feature_mgr.sh
@@ -4,13 +4,22 @@
 FEATURE_ROOT="$(realpath "$(dirname "$0")")"
 source "$(dirname "$0")/../internal/install-base.sh"
 
+install_centos() {
+  install_packages coreutils
+  _configure
+}
+
 install_ubuntu() {
   install_packages dircolors
-  ln -s "$FEATURE_ROOT"/config.dircolors "$HOME"/.dircolors
+  _configure
 }
 
 install_winbash() {
   install_ubuntu
+}
+
+_configure() {
+  ln -s "$FEATURE_ROOT"/config.dircolors "$HOME"/.dircolors
 }
 
 main "$@"
