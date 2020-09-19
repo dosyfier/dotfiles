ece4824d66da86c4717ea22a1fb77c9784b9305e
csouriss <christophe.sourisse@capgemini.com>
2020-09-19 19:35:03 +0200

    Enhanced install/upgrade procedures for Chocolatey
    In particular, a mecanism is added to allow managing different
    Chocolatey package configurations.

--- chocolatey/aliases/chocolatey.sh
+++ chocolatey/aliases/chocolatey.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-DOTBASHCFG_CHOCO_CONFIG=~/.bash/chocolatey/conf/packages.config
+DOTBASHCFG_CHOCO_CONFIG_DIR=~/.bash/chocolatey/conf
+DOTBASHCFG_CHOCO_CONFIG_DEFAULT="$(cat $DOTBASHCFG_CHOCO_CONFIG_DIR/default-config.txt)"
+
+_dotbashcfg_choco_inst_usage() {
+  available_types="$(find "$DOTBASHCFG_CHOCO_CONFIG_DIR" -name "packages-*.config" | \
+    sed 's/^.*-\([a-z]\+\)\..*$/\1/' | paste -sd ',' - | sed 's/,/, /g')"
+  echo "Usage: dotbashcfg_choco_inst [TYPE]"
+  echo "where TYPE may be: $available_types"
+  echo "(default: $DOTBASHCFG_CHOCO_CONFIG_DEFAULT)"
+  printf "\n"
+}
 
 dotbashcfg_choco_inst() {
-  choco install "$(wslpath $DOTBASHCFG_CHOCO_CONFIG)"
+  if [ $# -eq 1 ] && [[ "$1" =~ ^(-h|--help|-?)$ ]]; then
+    _dotbashcfg_choco_inst_usage
+    return
+  elif [ $# -gt 1 ]; then
+    _dotbashcfg_choco_inst_usage
+    return 1
+  fi
+
+  type="${1:-$DOTBASHCFG_CHOCO_CONFIG_DEFAULT}"
+  choco install "$(wslpath -w "$DOTBASHCFG_CHOCO_CONFIG_DIR\\packages-${type}.config")"
 }
 
 dotbashcfg_choco_up() {
--- chocolatey/install.bat
+++ chocolatey/install.bat
@@ -1,7 +1,37 @@
 @echo off
 
+REM Based on https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
+:check_permissions
+net session >nul 2>&1
+if not %errorLevel% == 0 (
+    echo Please run this script from an elevated shell.
+    goto :end
+)
+
+:init
+set /p choco_config=<%~dp0\conf\default-config.txt
+if not %errorLevel% == 0 goto :end
+goto :parse_opts
+
+:usage
+echo Usage: install.bat {CHOCO_CONFIG}
+echo where {CHOCO_CONFIG} may be "home" or "work".
+echo (default: %choco_config%)
+set errorlevel=1
+goto :end
+
+:parse_opts
+if not "%1" == "" if not "%1" == "home" if not "%1" == "work" goto :usage
+if not "%2" == "" goto :usage
+if not "%1" == "" set choco_config=%1
+goto :install_start
+
+:install_start
 WHERE choco.exe
-IF %ERRORLEVEL% EQU 0 goto :install_packages
+IF %errorlevel% EQU 0 (
+    echo Chocolatey detected. Skipping install.
+    goto :install_packages
+)
 
 :install_choco
 echo Installing Chocolatey...
@@ -9,9 +39,16 @@ echo Installing Chocolatey...
     -InputFormat None -ExecutionPolicy Bypass ^
     -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" ^
     && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+if not %errorLevel% == 0 goto :end
 
 :install_packages
-echo Installing Packages...
-choco install %~dp0\conf\packages.config
+echo Installing Packages (configuration set: %choco_config%)...
+choco install %~dp0\conf\packages-%choco_config%.config
+if not %errorLevel% == 0 goto :end
 
-IF %ERRORLEVEL% EQU 0 echo Done! Your Windows OS is fully configured!
+echo Done! Your Windows OS is fully configured!
+
+:end
+echo.
+exit /b %errorlevel%
+
