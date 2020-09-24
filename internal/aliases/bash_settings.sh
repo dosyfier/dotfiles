06ef1624519857ac473cfd462d6ab74d7a1e6ca2
csouriss <christophe.sourisse@capgemini.com>
2020-09-24 23:16:39 +0200

    [internal] Add $HOME/.local/bin to $PATH when suitable

--- internal/aliases/bash_settings.sh
+++ internal/aliases/bash_settings.sh
@@ -45,3 +45,8 @@ alias alert='notify-send --urgency=low '\
 '-i "$([ $? = 0 ] && echo terminal || echo error)" '\
 '"$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+# Add $HOME/.local/bin to $PATH when suitable
+if [ -d "$HOME/.local/bin" ] && ! [[ "$PATH" =~ $HOME/.local/bin ]]; then
+  PATH="$HOME/.local/bin:$PATH"
+fi
+
