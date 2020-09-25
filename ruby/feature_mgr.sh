35855b802a389933dcc21a613dfeb17ffa4d493e
csouriss <christophe.sourisse@capgemini.com>
2020-09-25 22:57:48 +0200

    [ruby] Configuring installation of my favorite gems

--- ruby/feature_mgr.sh
+++ ruby/feature_mgr.sh
@@ -5,6 +5,7 @@ source "$(dirname "$0")/../internal/install-base.sh"
 
 install_centos() {
   install_packages ruby
+  _install_gems
 }
 
 install_redhat() {
@@ -14,11 +15,24 @@ install_redhat() {
 install_ubuntu() {
   install_repo brightbox/ruby-ng
   install_packages ruby ruby-dev
+  _install_gems
 }
 
 install_wsl() {
   install_ubuntu
 }
-  
+
+_install_gems() {
+  sudo gem install \
+    solargraph \
+    chef \
+    knife \
+    rspec \
+    rubocop \
+    rake \
+    rdoc \
+    pry
+}
+
 main "$@"
 
