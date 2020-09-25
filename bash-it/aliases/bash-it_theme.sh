9085600632e7bff141e6ff1f0c4282b0fdd8c6a1
csouriss <christophe.sourisse@capgemini.com>
2020-09-25 23:00:41 +0200

    [bash-it] Install gitstatus to speed up Powerline prompt

--- bash-it/aliases/bash-it_theme.sh
+++ bash-it/aliases/bash-it_theme.sh
@@ -3,9 +3,16 @@
 EXTERNAL_PROMPT_ENABLED=true
 BASH_IT=/opt/bash-it
 
+if [ -d /opt/gitstatus ]; then
+  SCM_GIT_USE_GITSTATUS=true
+  source /opt/gitstatus/gitstatus.plugin.sh
+  gitstatus_start
+fi
+
 _command_exists() {
   type "$1" &> /dev/null;
 }
+
 source "$BASH_IT/themes/base.theme.bash"
 source "$BASH_IT/themes/colors.theme.bash"
 source "$BASH_IT/themes/githelpers.theme.bash"
--- bash-it/feature_mgr.sh
+++ bash-it/feature_mgr.sh
@@ -3,8 +3,6 @@
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
-BASH_IT_INSTALL_DIR=/opt/bash-it
-
 get_dependencies() {
   echo git
 }
@@ -13,11 +11,20 @@ install() {
   # shellcheck source=../git/aliases/git.sh
   source "$DOTBASH_CFG_ROOT/git/aliases/git.sh"
 
-  if [ -e "$BASH_IT_INSTALL_DIR" ]; then
-    git_stash_n_pull "$BASH_IT_INSTALL_DIR" pull
+  _install_or_update_git_repo "https://github.com/Bash-it/bash-it.git" \
+    "/opt/bash-it"
+  _install_or_update_git_repo "https://github.com/romkatv/gitstatus.git" \
+    "/opt/gitstatus"
+}
+
+_install_or_update_git_repo() {
+  git_repo_url="$1"
+  git_repo_dir="$2"
+  if [ -e "$git_repo_dir" ]; then
+    git_stash_n_pull "$git_repo_dir" pull
   else
-    sudo git clone https://github.com/Bash-it/bash-it.git "$BASH_IT_INSTALL_DIR"
-    sudo chown -R "$USER" "$BASH_IT_INSTALL_DIR"
+    sudo git clone "$git_repo_url" 
+    sudo chown -R "$USER" "$git_repo_dir"
   fi
 }
 
