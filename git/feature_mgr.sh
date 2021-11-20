bf6f44e2a06a8625cdadad877636ddd85e6be419
Christophe Sourisse <christophe.sourisse@gmail.com>
2021-11-20 12:32:28 +0100

    [git] Unlock git upgrade on Ubuntu/WSL

--- git/feature_mgr.sh
+++ git/feature_mgr.sh
@@ -7,7 +7,7 @@ FEATURE_ROOT="$(readlink -f "$(dirname "$0")")"
 GIT_SCL_VERSION=2.18
 
 install_centos() {
-  if ! _is_installed; then
+  if ! _is_scl_or_upper_installed; then
     install_repo centos-release-scl epel-release
     _install_rh_git
   fi
@@ -15,7 +15,7 @@ install_centos() {
 }
 
 install_redhat() {
-  if ! _is_installed; then
+  if ! _is_scl_or_upper_installed; then
     install_repo rhel-server-rhscl-7-rpms
     _install_rh_git
   fi
@@ -30,10 +30,8 @@ _install_rh_git() {
 }
 
 install_ubuntu() {
-  if ! _is_installed; then
-    install_repo git-core/ppa
-    install_packages git gitk
-  fi
+  install_repo git-core/ppa
+  install_packages git gitk
   _configure
 }
 
@@ -45,14 +43,10 @@ _format_version_for_cmp() {
   echo "$1" | sed 's|\.| |g' | xargs printf "%02d.%02d" 
 }
 
-_is_installed() {
+_is_scl_or_upper_installed() {
   installed_git_version=$(git --version 2> /dev/null | awk '{ print $NF }')
-  if [ -n "$installed_git_version" ] && \
-    [[ "$(_format_version_for_cmp "$installed_git_version")" > "$(_format_version_for_cmp "$GIT_SCL_VERSION")" ]]; then
-    return 0
-  else
-    return 1
-  fi
+  [ -n "$installed_git_version" ] && \
+    [[ "$(_format_version_for_cmp "$installed_git_version")" > "$(_format_version_for_cmp "$GIT_SCL_VERSION")" ]]
 }
 
 _configure() {
