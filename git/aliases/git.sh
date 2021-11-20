47b82988cf8b6a9f62e2a4ed11c0dbc81158e223
csouriss <christophe.sourisse@capgemini.com>
2021-11-20 14:28:03 +0100

    [git] New tooling functions added

--- git/aliases/git.sh
+++ git/aliases/git.sh
@@ -54,6 +54,54 @@ git_prompt_command() {
   DOTBASHCFG_LAST_PWD="$new_pwd"
 }
 
+# Run a diff-like git command and ignore any difference that is white-space related
+git_no_white() {
+  git -c "core.whitespace=-trailing-space,-indent-with-non-tab,-tab-in-indent" "$1" \
+    -U0 --word-diff-regex='[^[:space:]]' "${@:2}"
+}
+
+# Completely erase some file(s) from git history
+git_erase_from_history() {
+  (
+    set -eo pipefail
+    git filter-branch  --prune-empty \
+      --index-filter "git rm -rf --cached --ignore-unmatch $*" \
+      --tag-name-filter cat -- --all
+    git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
+    git reflog expire --expire=now --all
+    git gc --aggressive --prune=now
+  )
+}
+
+# List most sizeable files within the current git repository
+git_ls_big_files() {
+  for arg in "$@"; do
+    if [[ "$arg" =~ ^--min-size= ]]; then
+      min_size="${arg#*=}"
+      size_filter_cmd="awk '\$2 >= $min_size*2^20'"
+    elif [ "$arg" = --ignore-current-tree ]; then
+      tree_filter_cmd="grep -vF --file=<(git ls-tree -r HEAD | awk '{print \$3}')"
+    else
+      echo "Usage: git_ls_big_files [--min-size=<size_in_MiB>] [--ignore-current-tree]"
+      return 1
+    fi
+  done
+
+  git rev-list --objects --all | \
+    git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' | \
+    sed -n 's/^blob //p' | \
+    eval $(if [ -v tree_filter_cmd ]; then echo "$tree_filter_cmd"; else echo cat; fi) | \
+    eval $(if [ -v size_filter_cmd ]; then echo "$size_filter_cmd"; else echo cat; fi) | \
+    sort --numeric-sort --key=2 | \
+    cut -c 1-12,41- | \
+    $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+
+  unset tree_filter_cmd size_filter_cmd
+}
+
+# Grep files within a git repository, without looking into the .git directory (which may take some time...)
+alias grep_git="grep --exclude-dir=.git"
+
 # If a bash prompt is defined by a theme external to dotbashconfig (e.g. Bash-It's Powerline),
 # then this alias script is ignored
 if [ "$EXTERNAL_PROMPT_ENABLED" = true ]; then
