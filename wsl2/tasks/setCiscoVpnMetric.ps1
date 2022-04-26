1b212771f0bec833de890b04c1196c5bbe8ee1d0
csouriss <christophe.sourisse@capgemini.com>
2022-04-26 23:20:28 +0200

    fix(wsl2): Let Cisco VPN metric task script run on company computers

--- wsl2/tasks/setCiscoVpnMetric.ps1
+++ wsl2/tasks/setCiscoVpnMetric.ps1
@@ -1,3 +1,10 @@
-Get-NetAdapter |
-  Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} |
-  Set-NetIPInterface -InterfaceMetric 6000
+# Administrator permissions are required to update an IP interface
+# Thus, we start a new elevated PowerShell process
+# (unfortunately, this can't be done by configuring the scheduled task on company computers...)
+Start-Process -FilePath powershell.exe -Verb runAs -ArgumentList (
+"Write-Host 'Reconfiguring Cisco VPN interface...'; " +
+"Get-NetAdapter " +
+"  | Where-Object {`$_.InterfaceDescription -Match 'Cisco AnyConnect'} " +
+"  | Set-NetIPInterface -InterfaceMetric 6000; " +
+"Write-Host 'Cisco VPN interface reconfigured.'; "
+)
