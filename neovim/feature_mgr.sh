dd8842d36d7be853b8756be4e3cec4af6be7b83b
Christophe Sourisse <christophe.sourisse@gmail.com>
2025-02-18 15:17:44 +0100

    feat(neovim): Support update for Neovim feature

--- neovim/feature_mgr.sh
+++ neovim/feature_mgr.sh
@@ -16,21 +16,25 @@ get_dependencies() {
   fi
 }
 
-install_ubuntu() {
-  _install
+_download_and_install() {
+  sudo bash -c "curl -sSfL '$NEOVIM_DOWNLOAD_URL' | tar -C $DOTFILES_TOOLS_DIR -xz"
 }
 
-install_wsl() {
-  _install
-}
-
-_install() {
-  sudo bash -c "curl -L '$NEOVIM_DOWNLOAD_URL' | tar -C $DOTFILES_TOOLS_DIR -xz"
-  ln -s "$FEATURE_ROOT/conf" "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+install_common() {
+  _download_and_install
+  ln -sfn "$FEATURE_ROOT/conf" "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 }
 
 get_resources() {
   _get_installed_resource "$HOME/.config/nvim" "$HOME/.local/share/nvim"
+}
+
+update() {
+  if command -v nvim &> /dev/null && [ "$(nvim -v | head -n 1 | awk '{print $NF}')" = $NEOVIM_VERSION ]; then
+    echo "Neovim is up-to-date (version: $NEOVIM_VERSION). Skipping."
+  else
+    _download_and_install
+  fi
 }
 
 main "$@"
