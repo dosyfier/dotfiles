bc9488288d5ef6b203a8a7598dabdbcdbafe62a1
Christophe Sourisse <christophe.sourisse@gmail.com>
2025-04-03 23:22:43 +0200

    feat(neovim): Upgrade to Neovim v0.11.0 & fzf v0.61.0

--- neovim/feature_mgr.sh
+++ neovim/feature_mgr.sh
@@ -3,11 +3,11 @@
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
-NEOVIM_VERSION=0.10.4
-NEOVIM_ARCHIVE_NAME=nvim-linux64.tar.gz
+NEOVIM_VERSION=0.11.0
+NEOVIM_ARCHIVE_NAME=nvim-linux-x86_64.tar.gz
 NEOVIM_DOWNLOAD_URL=https://github.com/neovim/neovim/releases/download/v$NEOVIM_VERSION/$NEOVIM_ARCHIVE_NAME
 
-FZF_VERSION=0.60.0
+FZF_VERSION=0.61.0
 FZF_ARCHIVE_NAME=fzf-$FZF_VERSION-linux_amd64.tar.gz
 FZF_DOWNLOAD_URL=https://github.com/junegunn/fzf/releases/download/v$FZF_VERSION/$FZF_ARCHIVE_NAME
 
