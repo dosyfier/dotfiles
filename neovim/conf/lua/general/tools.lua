1d09d3b99d536e90cb3d80f39ce328c8936820d0
Christophe Sourisse <christophe.sourisse@gmail.com>
2025-04-04 00:58:32 +0200

    fix(neovim): Add a key binding to SynGroup function

--- neovim/conf/lua/general/tools.lua
+++ neovim/conf/lua/general/tools.lua
@@ -1,9 +1,10 @@
 -- Displays the syntax highlight group (and link, if there is one)
 -- of the word at the current cursor position
 -- Cf. https://stackoverflow.com/a/37040415/4601482
-vim.api.nvim_exec([[
-function! SynGroup()
-    let l:s = synID(line('.'), col('.'), 1)
-    echo synIDattr(l:s, 'name') . ' -> ' . synIDattr(synIDtrans(l:s), 'name')
-endfunc
-]], false)
+function SynGroup()
+  vim.cmd([[
+    let syn_group = synID(line('.'), col('.'), 1)
+    echo synIDattr(syn_group, 'name') . ' -> ' . synIDattr(synIDtrans(syn_group), 'name')
+  ]])
+end
+vim.keymap.set('n', '<Leader>y', SynGroup, { desc = 'Display syntax highlight group' })
