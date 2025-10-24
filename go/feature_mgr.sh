75388be97b5e398b219a52dd86c1acc72c1f68c0
Christophe Sourisse <christophe.sourisse@fr.airbus.com>
2025-10-24 16:44:18 +0200

    fix(go): Use up-to-date go version + better error handling during install

--- go/feature_mgr.sh
+++ go/feature_mgr.sh
@@ -3,13 +3,13 @@
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
-GO_VERSION=1.22.3
+GO_VERSION=1.25.3
 GO_ARCHIVE_NAME=go${GO_VERSION}.linux-amd64.tar.gz
 GO_DOWNLOAD_URL=https://go.dev/dl/$GO_ARCHIVE_NAME
 
 install_common() {
-  remove_or_abort /opt/go sudo
-  sudo bash -c "curl -L '$GO_DOWNLOAD_URL' | tar -C /opt -xz"
+  remove_or_abort "$DOTFILES_TOOLS_DIR/go" sudo
+  sudo bash -c "curl -sSfL '$GO_DOWNLOAD_URL' | tar -C '$DOTFILES_TOOLS_DIR' -xz"
 }
 
 main "$@"
