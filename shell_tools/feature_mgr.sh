64078d5a5b63e9b3e9eb65819dfd7413309ad806
Christophe Sourisse <christophe.sourisse@fr.airbus.com>
2025-12-07 11:35:39 +0100

    feat(shell_tools): Support shell_tools install on any RHEL-like env

--- shell_tools/feature_mgr.sh
+++ shell_tools/feature_mgr.sh
@@ -3,23 +3,7 @@
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
-install_centos() {
-  _install
-}
-
-install_redhat() {
-  _install
-}
-
-install_ubuntu() {
-  _install
-}
-
-install_wsl() {
-  _install
-}
-
-_install() {
+install_common() {
   install_packages rsync tmux tree xclip
 }
 
