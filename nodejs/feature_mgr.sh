80e27ca96429cb9bc005ad1f4082cc6e2a3dbb1e
Christophe Sourisse <christophe.sourisse@fr.airbus.com>
2025-12-14 10:54:52 +0100

    feat(nodejs): Actually install NodeJS, not only NVM

--- nodejs/feature_mgr.sh
+++ nodejs/feature_mgr.sh
@@ -2,12 +2,25 @@
 
 NVM_VERSION=0.40.3
 
+NODEJS_VERSION=24.11.1
+
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
 install_common() {
+  # Install NVM
   curl -fsSL -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh" | \
     SHELL=noshell bash
+
+  # Load NVM
+  source "$(dirname "$0")/aliases/nodejs.sh"
+  load_nvm
+
+  # Install configured Node.js version
+  if [ -z "$(nvm ls --no-alias --no-colors 2>/dev/null)" ]; then
+    nvm install "$NODEJS_VERSION"
+  fi
+  nvm use default
 }
 
 main "$@"
