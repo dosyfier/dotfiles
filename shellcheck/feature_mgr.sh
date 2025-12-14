e0c1db246a92e6ec42921c2304bf1a1dec817233
Christophe Sourisse <christophe.sourisse@fr.airbus.com>
2025-12-14 13:33:33 +0100

    feat(shellcheck): Install from GitHub pre-built releases

--- shellcheck/feature_mgr.sh
+++ shellcheck/feature_mgr.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
 
-_SHELLCHECK_VERSION=0.8.0
-_SHELLCHECK_SRC_DIR=/usr/local/src/shellcheck
+SHELLCHECK_VERSION=0.11.0
 
 # shellcheck source=../internal/install-base.sh
 source "$(dirname "$0")/../internal/install-base.sh"
 
-get_dependencies() {
-  echo cabal
-  if ! command -v git > /dev/null 2>&1; then
-    echo git
-  fi
-}
+install_common() {
+  install_packages xz
 
-install_centos() {
-  _install
-}
+  shellcheck_download_url="https://github.com/koalaman/shellcheck"
+  shellcheck_download_url+="/releases/download/v$SHELLCHECK_VERSION"
+  shellcheck_download_url+="/shellcheck-v$SHELLCHECK_VERSION.linux.x86_64.tar.xz"
 
-install_redhat() {
-  _install
-}
-
-install_ubuntu() {
-  _install
-}
-
-install_wsl() {
-  _install
-}
-
-_install() {
-  if command -v shellcheck > /dev/null 2>&1 && \
-    [ "$(shellcheck --version | awk '/version:/ { print $NF; quit }')" = "$_SHELLCHECK_VERSION" ]; then
-    echo "ShellCheck is already installed in version $_SHELLCHECK_VERSION. Skipping."
-
-  else
-    # Re-update cabal (when the feature is played indepently, without the "cabal" feature)
-    sudo cabal update
-
-    # Clone ShellCheck official git repository and switch to the desired branch / tag
-    if ! [ -d "$_SHELLCHECK_SRC_DIR" ]; then
-      sudo git clone https://github.com/koalaman/shellcheck "$_SHELLCHECK_SRC_DIR"
-      sudo git -C "$_SHELLCHECK_SRC_DIR" checkout "v${_SHELLCHECK_VERSION}"
-    fi
-
-    # Install ShellCheck globally
-    sudo bash -c "cd $_SHELLCHECK_SRC_DIR; cabal install --global"
-  fi
+  ensure_dir_exists "$DOTFILES_LOCAL_DIR/bin"
+  run_lenient_sudo bash -c "curl -sSfL '$shellcheck_download_url' | \
+tar -xJ -C '$DOTFILES_LOCAL_DIR/bin' --strip-components=1 shellcheck-v$SHELLCHECK_VERSION/shellcheck"
 }
 
 main "$@"
-
