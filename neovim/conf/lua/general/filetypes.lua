bd22cf3991d9df99bbf4ebd432915ef80f27a0da
Christophe Sourisse <christophe.sourisse@fr.airbus.com>
2025-12-16 15:47:48 +0100

    fix(neovim): Effectively trigger linting on GitLab CI yml files

--- neovim/conf/lua/general/filetypes.lua
+++ neovim/conf/lua/general/filetypes.lua
@@ -1,4 +1,15 @@
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {"*/playbooks/*.yml","*/playbooks/*.yaml","play-*.yml","play-*.yaml"},
+  pattern = {
+    "*/playbooks/*.{yml,yaml}",
+    "play-*.{yml,yaml}"
+  },
   command = "set filetype=yaml.ansible"
+})
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = {
+    ".gitlab-ci.{yml,yaml}",
+    ".gitlab-ci/*.{yml,yaml}",
+    "templates/gitlab-ci-*.{yml,yaml}"
+  },
+  command = "set filetype=yaml.gitlab"
 })
